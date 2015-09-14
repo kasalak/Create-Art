39b4af2df385daa7dd80bde62b625d28394eded9
Karthik <karthik.kasala@gmail.com>
2015-09-14 17:49:23 -0400

    finished normal mode

--- random_art.py
+++ random_art.py
@@ -1,16 +1,22 @@
 import random
+import math
+from math import *
 
 # Your job is to create better version of create_expression and
 # run_expression to create random art.
 # Your expression should have a __str__() function defined for it.
 
+def avg(x, y):
+    return (x+y) / 2
+
 
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    expr1 = lambda x, y: (x + y)/2
+
+    expr1 = lambda x, y: ((cos(x) - cos(y)) * random.random())
     expr2 = lambda x, y: (x - y)/2
-    expr3 = lambda x, y: x * y
+    expr3 = lambda x, y: (x + y)/2
     return random.choice([expr1, expr2, expr3])
 
 
